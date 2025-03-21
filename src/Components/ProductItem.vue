34ee41ad03999f6bf72e91490ccbb21241ea2f8e
sezerr <Aleksandra.Golets.2017@yandex.ru>
2025-03-22 02:43:00 +0300

    save

---
+++ src/Components/ProductItem.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+  name: "ProductItem"
+}
+</script>
+
+<template>
+<div class="main_wrapper"></div>
+</template>
+
+<style scoped>
+.main_wrapper{
+  background-color: #D9D9D9;;
+  border-radius: 27px;
+  width: 910px;
+  height: 100px;
+}
+</style>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,10 +1,12 @@
 <script>
 import router from "@/router/router.js";
+import ProductItem from "@/Components/ProductItem.vue";
 
 export default {
-  methods:{
-    routerPushtoHome(){
-      router.push({path:"/"})
+  components: {ProductItem},
+  methods: {
+    routerPushtoHome() {
+      router.push({path: "/"})
     }
   }
 }
@@ -12,12 +14,82 @@ export default {
 
 <template>
   <div>
-    <H1>Продукты</H1>
-    <button style="height: 50px;width: 250px;background-color: red;border-radius: 25px" @click="routerPushtoHome">На главную</button>
+    <H1 class="title">Products</H1>
+    <div class="search_box">
+      <input type="text" class="search">
+    </div>
+    <div class="titling">
+      <div class="titling_item">
+        <p>Name</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+      <div class="titling_item">
+        <p>Count</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+      <div class="titling_item">
+        <p>Group</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+      <div class="titling_item">
+        <p>Number</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+      <div class="titling_item">
+        <p>Manufacturer</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+      <div class="titling_item">
+        <p>Price</p>
+        <img src="../../public/img/down_arrow.svg" alt="стрелочка вниз">
+      </div>
+    </div>
+    <div class="wrapper">
+      <ProductItem></ProductItem>
+    </div>
+    <button style="height: 50px;width: 250px;background-color: red;border-radius: 25px" @click="routerPushtoHome">На
+      главную
+    </button>
 
   </div>
 </template>
 
 <style scoped>
+.title {
+  margin-top: 50px;
+  margin-left: 60px;
+  font-size: 36px;
+  font-weight: 600;
+  font-family: 'Inter-mf', sans-serif;
+}
 
+.search {
+  width: 910px;
+  height: 44px;
+  background-color: #D9D9D9;
+  border-radius: 87px;
+
+}
+
+.search_box {
+  margin-top: 8px;
+  margin-left: 60px;
+}
+.titling{
+  margin-top: 15px;
+  margin-left: 60px;
+  display:flex;
+  width: 910px;
+  justify-content: space-around;
+}
+.titling_item{
+  display: flex;
+  font-size: 20px;
+  font-weight: 600;
+  font-family: 'Inter-mf', sans-serif;
+}
+.wrapper{
+  margin-left: 60px;
+  margin-top: 15px;
+}
 </style>
